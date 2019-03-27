b0522eb26a0a3cb20e8d156e1f67555437044dd5
Aminuddin <amidia.amin@gmail.com>
2019-03-27 17:23:32 +0700

    initial commit

---
+++ src/Espay.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Aminuddin <sinaunengweb@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace Espay;
+namespace spengembang\Espay;
+
+class Espay
+{
+    public function Inquiry($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
